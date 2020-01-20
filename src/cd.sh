3a07f71e0bcd26be87bcb7483249228f2cbaeea4
mc36 <mc36@freemail.hu>
2020-01-20 09:48:53 +0100

    automatic commit at releng box

--- src/cd.sh
+++ src/cd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./d.sh
 echo generating
-javadoc -private -source 1.7 -use -linksource -keywords -quiet -d ../binTmp/ router.java
+javadoc -private -source 8 -use -linksource -keywords -quiet -d ../binTmp/ router.java
 #doxygen ../misc/doxygen.cfg
-###cd .. ; find src -name *.java -and -type f -print0 | xargs -s 512000 -0 /usr/lib/jvm/java-10-openjdk-amd64/bin/javadoc -quiet -d binTmp/ -source 1.7
+###cd .. ; find src -name *.java -and -type f -print0 | xargs -s 512000 -0 /usr/lib/jvm/java-14-openjdk-amd64/bin/javadoc -quiet -d binTmp/ -source 8
