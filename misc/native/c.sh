b165605485f59fff1b9284ec809a0ee46830a0e2
mc36 <mc36@freemail.hu>
2020-06-25 17:03:20 +0200

    automatic commit at releng box

--- misc/native/c.sh
+++ misc/native/c.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-LIBS="-lpthread -lpcap -lrte_eal -lrte_mempool -lrte_mbuf -lrte_ring -lrte_ethdev"
-INCS="-I /usr/include/dpdk/ -I /usr/include/x86_64-linux-gnu/dpdk"
 CC="clang"                              #clang
 CC="gcc"                                #gcc
 
-for fn in p4pkt p4emu p4dpdk mapInt rawInt pcapInt tapInt bundle vlan hdlcInt stdLin ttyLin modem; do
-  echo compiling $fn.
-  $CC -O3 -march=corei7 -o../../binTmp/$fn.bin $INCS $fn.c $LIBS
+compileFile()
+{
+echo compiling $1.
+$CC -O3 -march=corei7 -o../../binTmp/$1.bin $2 $1.c $3
+}
+
+compileFile p4dpdk "-I /usr/include/dpdk/ -I /usr/include/x86_64-linux-gnu/dpdk" "-lpthread -lpcap -lrte_eal -lrte_mempool -lrte_mbuf -lrte_ring -lrte_ethdev"
+for fn in p4pkt p4emu mapInt rawInt pcapInt tapInt bundle vlan hdlcInt stdLin ttyLin modem; do
+  compileFile $fn "" "-lpthread -lpcap"
   done
