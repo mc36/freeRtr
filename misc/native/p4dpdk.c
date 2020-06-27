fbac428df21f848d9ef5cd08edc645d2249b554b
mc36 <mc36@freemail.hu>
2020-06-27 16:45:16 +0200

    automatic commit at releng box

--- misc/native/p4dpdk.c
+++ misc/native/p4dpdk.c
@@ -139,20 +139,18 @@ static int doPacketLoop(__rte_unused void *arg) {
         for (seq = 0; seq < myconf->num_port; seq++) {
             port = myconf->port_list[seq];
             num = rte_ring_count(tx_ring[port]);
-            if (num > 0) {
-                if (num > BURST_SIZE) num = BURST_SIZE;
-                num = rte_ring_sc_dequeue_bulk(tx_ring[port], (void**)bufs, num, NULL);
-                rte_eth_tx_burst(port, 0, bufs, num);
-                pkts += num;
-            }
-            num = rte_eth_rx_burst(port, 0, bufs, BURST_SIZE);
+            if (num > BURST_SIZE) num = BURST_SIZE;
+            num = rte_ring_sc_dequeue_bulk(tx_ring[port], (void**)bufs, num, NULL);
+            rte_eth_tx_burst(port, 0, bufs, num);
             pkts += num;
+            num = rte_eth_rx_burst(port, 0, bufs, BURST_SIZE);
             for (i = 0; i < num; i++) {
                 bufS = rte_pktmbuf_pkt_len(bufs[i]);
                 memmove(&bufD[preBuff], rte_pktmbuf_mtod(bufs[i], void *), bufS);
                 rte_pktmbuf_free(bufs[i]);
                 if (port == cpuport) processCpuPack(&bufD[0], bufS); else processDataPacket(&bufD[0], bufS, port);
             }
+            pkts += num;
         }
         if (pkts < 1) usleep(100);
     }
