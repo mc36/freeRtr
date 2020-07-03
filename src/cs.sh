281f05df552a49dac984e7e06bbdb403f0b77b47
mc36 <mc36@freemail.hu>
2020-07-03 11:09:00 +0200

    automatic commit at releng box

--- src/cs.sh
+++ src/cs.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo signing
 java -Xmx256m -jar rtr.jar show version number | dos2unix | head -n 1 > rtr.txt
+echo -n "version="
+cat rtr.txt
 java -Xmx256m -jar rtr.jar test verfile http://10.5.1.10/freertr2.key rtr.zip@../rtr.zip
 java -Xmx256m -jar rtr.jar test vercore http://10.5.1.10/freertr2.key http://10.5.1.10/freertr2.key
 echo verifying
