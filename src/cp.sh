4305756cc67d9b0ee81ba5992e975958f51bbfb1
mc36 <mc36@freemail.hu>
2020-07-11 16:31:44 +0200

    automatic commit at releng box

--- src/cp.sh
+++ src/cp.sh
@@ -4,9 +4,9 @@ TRG=../binOut/
 #jar cfm0 rtr.jar manifest.txt -C $TRG .
 mkdir -p $TRG/META-INF
 cp manifest.txt $TRG/META-INF/MANIFEST.MF
-PWD=`pwd`
+CWD=`pwd`
 cd $TRG
 LST=`find . | sort`
 touch -d "2010-01-01 00:00:00" $LST
-zip -0  ../src/rtr.jar $LST > /dev/null
-cd $PWD
+zip -0 $CWD/rtr.jar $LST > /dev/null
+cd $CWD
