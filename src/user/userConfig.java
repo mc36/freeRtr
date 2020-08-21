0630967ee5694f78bf85be9e2681671cd00350c0
mc36 <mc36@freemail.hu>
2020-08-21 15:11:03 +0200

    automatic commit at releng box

--- src/user/userConfig.java
+++ src/user/userConfig.java
@@ -2614,6 +2614,9 @@ public class userConfig {
         l.add("4  5          <network>                    destination network");
         l.add("5  6            <mask>                     destination mask");
         l.add("6  7,.            <nexthop>                forwarding router's address");
+        l.add("7  7,.              recurigp               use recursive nexthop");
+        l.add("7  7,.              recurbgp               use recursive nexthop");
+        l.add("7  7,.              recurvpn               use recursive nexthop");
         l.add("7  8                distance               set distance metric");
         l.add("8  7,.                <dist>               distance value");
         l.add("7  8                tag                    set tag value");
