0e5ab772f36aa8b2f08b52c267fb1a304fc8b4cd
mc36 <mc36@freemail.hu>
2020-10-04 15:36:02 +0200

    automatic commit at releng box

--- src/tb.sh
+++ src/tb.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./c.sh
 ./cb.sh
-java -Xmx256m -jar rtr.jar test tester - binary summary retry 16 url http://sources.nop.hu/cfg/ $1 $2 $3 $4 $5 $6 $7 $8
+java -Xmx256m -jar rtr.jar test tester - binary summary slot 1 retry 16 url http://sources.nop.hu/cfg/ $1 $2 $3 $4 $5 $6 $7 $8
