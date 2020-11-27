ae99af326fb98c4395197b39addc0ef485a81a66
mc36 <mc36@freemail.hu>
2020-11-27 15:02:43 +0100

    automatic commit at releng box

--- misc/p4bf/c-all.sh
+++ misc/p4bf/c-all.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #sudo apt install bf_sde psmisc iproute2 net-tools socat tshark iperf gcc telnet
 #git clone ssh://git@bitbucket.software.geant.org:7999/rare/rare.git
+#git clone ssh://git@bitbucket.software.geant.org:7999/rare/rare-bf2556x-1t.git
 #gcc -O3 -o cons.bin cons.c
 #cp initd /etc/init.d/rtr
 #chmod 755 /etc/init.d/rtr
