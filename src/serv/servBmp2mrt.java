fa8264d3fcd49a9d514bb9b58e151a17908558f1
mc36 <mc36@freemail.hu>
2020-12-04 07:58:56 +0100

    automatic commit at releng box

--- src/serv/servBmp2mrt.java
+++ src/serv/servBmp2mrt.java
@@ -342,7 +342,7 @@ public class servBmp2mrt extends servGeneric implements prtServS {
         }
         servBmp2mrtStat old = stats.add(res);
         if (old != null) {
-            if (old.as == 0) {
+            if (as != 0) {
                 old.as = as;
             }
             return old;
