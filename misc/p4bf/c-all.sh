1fc47cf1a3d68bc16c3e0f71f30d8ce6ca3cf461
mc36 <mc36@freemail.hu>
2021-02-09 20:17:25 +0100

    automatic commit at releng box

--- misc/p4bf/c-all.sh
+++ misc/p4bf/c-all.sh
@@ -5,6 +5,7 @@
 #rm -rf $SDE/p4studio_build/p4studio_logs
 #rm -rf $SDE/p4studio_build/third_party
 #rm -rf $SDE/packages
+#
 #git clone ssh://git@bitbucket.software.geant.org:7999/rare/rare.git
 #git clone ssh://git@bitbucket.software.geant.org:7999/rare/rare-bf2556x-1t.git
 #gcc -O3 -o cons.bin cons.c
@@ -14,12 +15,20 @@
 #systemctl mask serial-getty@ttyS0
 #systemctl disable serial-getty@ttyS0
 #echo "mc36 ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+#
+#fdisk /dev/sdb / p
+#fsck -f /dev/sdb1
+#resize2fs /dev/sdb1 2097152
+#fsck -f /dev/sdb1
+#fdisk /dev/sdb / p d n p 1 2048 16779264 n w
+#qemu-img resize --shrink p4bf.img 8390144K
+#
 cd /home/mc36/rare/p4src
 export SDE=/home/mc36/bf-sde-9.4.0
 export SDE_INSTALL=/home/mc36/bf-sde-9.4.0/install
 #$SDE/install/bin/bf-p4c bf_router.p4
 sudo -E $SDE/tools/p4_build.sh -I. $@ ./bf_router.p4
 cd $SDE/logs/p4-build/bf_router
-sudo csplit make.log /bf-p4c/ /running/
+sudo csplit make.log /p4c/ /p4c/
 tail -n+2 xx01
 sudo rm xx0*
