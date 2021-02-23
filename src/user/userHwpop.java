63bd0a70d213ad086095033628a0ec077e8d9ab1
mc36 <mc36@freemail.hu>
2021-02-23 05:43:26 +0100

    automatic commit at releng box

--- src/user/userHwpop.java
+++ src/user/userHwpop.java
@@ -198,7 +198,7 @@ public class userHwpop {
                 }
             }
             txt2.add("interface sdn" + sdn);
-            txt2.add(cmds.tabulator + "description front port " + ntry.desc);
+            txt2.add(cmds.tabulator + "description frontpanel port " + ntry.desc);
             txt2.add(cmds.tabulator + cmds.finish);
             txt2.add(cmds.comment);
             txt.add(cmds.tabulator + "export-port sdn" + sdn + " " + ntry.port + " " + spd);
