0a46c26eaa65aad361037fe82d75dc86af75acca
mc36 <mc36@freemail.hu>
2021-08-13 11:06:10 +0200

    automatic commit at releng box

--- misc/p4bf/c-all.sh
+++ misc/p4bf/c-all.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #sudo apt install bf_sde psmisc iproute2 net-tools socat tshark iperf gcc git telnet
-#$SDE/p4studio_build/p4studio_build.py
+#$SDE/p4studio_build/p4studio profile apply ./profiles/p4-runtime-tofino.yaml
 #rm -rf $SDE/build
-#rm -rf $SDE/p4studio_build/p4studio_logs
-#rm -rf $SDE/p4studio_build/third_party
+#rm -rf $SDE/p4studio/dependencies
 #rm -rf $SDE/packages
 #rm -f `find $SDE -name *.a`
 #
@@ -26,7 +25,7 @@
 #fallocate -d p4bf.img
 #
 cd /home/mc36/rare/p4src
-export SDE=/home/mc36/bf-sde-9.4.0
+export SDE=/home/mc36/bf-sde-9.6.0
 export SDE_INSTALL=$SDE/install
 #$SDE/install/bin/bf-p4c -I. $@ bf_router.p4
 sudo -E $SDE/tools/p4_build.sh -I. $@ ./bf_router.p4
