c092cdbcdc33392f52318d47d4ed6c32f9dfaa8e
mc36 <mc36@freemail.hu>
2021-08-24 08:18:55 +0200

    automatic commit at releng box

--- src/cb.sh
+++ src/cb.sh
@@ -2,4 +2,4 @@
 echo compiling
 cp rtr.jar rtr2.jar
 zip -d rtr2.jar "pipe/pipeWindow*"
-native-image --no-server --allow-incomplete-classpath --no-fallback --enable-all-security-services -jar rtr2.jar rtr.bin
+native-image --allow-incomplete-classpath --no-fallback --enable-all-security-services -jar rtr2.jar rtr.bin
