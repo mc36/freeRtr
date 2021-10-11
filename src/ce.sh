e49d576cc0a3a8184ce0bafc3f5e230b5f736c09
mc36 <mc36@freemail.hu>
2021-10-11 06:43:40 +0200

    automatic commit at releng box

--- src/ce.sh
+++ src/ce.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo compiling
-ecj -source 11 -target 11 -Xlint:all -deprecation -d ../binOut/ -sourcepath ./ *.java net/freertr/*.java
+ecj @compiler.txt
--- src/cj.sh
+++ src/cj.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo compiling
-javac -source 11 -target 11 -Xlint:all -deprecation -d ../binOut/ -sourcepath ./ *.java net/freertr/*.java
+javac @compiler.txt
