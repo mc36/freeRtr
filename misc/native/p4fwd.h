7cc5a3c4de3684fa6f1b4a089d29ea781afd34fe
mc36 <mc36@freemail.hu>
2021-12-21 07:45:30 +0100

    automatic commit at releng box

--- misc/native/p4fwd.h
+++ misc/native/p4fwd.h
@@ -1111,7 +1111,7 @@ etyped_rx:
     case ETHERTYPE_MPLS_UCAST: // mpls
         checkLayer2;
         if (index < 0) doDropper;
-        if (portvrf_res->mpls == 0) doPunting;
+////        if (portvrf_res->mpls == 0) doPunting;
         packMpls[port]++;
         byteMpls[port] += bufS;
 mpls_rx:
