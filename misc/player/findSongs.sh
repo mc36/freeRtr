0b6cdeb4554e7bb69ccb728ffaac6487b7e834ff
mc36 <mc36@freemail.hu>
2022-01-13 09:11:51 +0100

    automatic commit at releng box

--- misc/player/findSongs.sh
+++ misc/player/findSongs.sh
@@ -2,7 +2,8 @@
 java findSongs /nfs/temp player-new.pls player-new.pls found1.pls
 java findSongs /nfs/musica player-cls.pls player-cls.pls found2.pls
 java findSongs /nfs/music player-all.pls player-all.pls found3.pls
-cat found1.pls found2.pls found3.pls > lyr-need.pls
+cat found1.pls found2.pls found3.pls > lyr-needed.pls
 rm found1.pls found2.pls found3.pls
-sed 's+=/nfs/+=/nfs2/+g' lyr-need.file
+cat lyr-needed.pls >> player-new.pls
+sed 's+=/nfs/+=/nfs2/+g' lyr-needed.pls > lyr-need.pls
 java lyrFetch
