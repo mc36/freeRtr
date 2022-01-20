6694283520421db90e4db8b2f231c5f7f3724ed0
mc36 <mc36@freemail.hu>
2022-01-20 22:38:30 +0100

    automatic commit at releng box

--- misc/p4bf/update.sh
+++ misc/p4bf/update.sh
@@ -8,6 +8,7 @@ cd ~
 mkdir a
 wget -O a/rtr.zip src.mchome.nop.hu/rtr.zip
 unzip a/rtr.zip -d a/
+sudo chown -R mc36:mc36 *
 rm -rf rare/p4src/include/
 rm -rf rare/bfrt_python/rare/
 mv a/misc/p4bf/*.p4 rare/p4src/
