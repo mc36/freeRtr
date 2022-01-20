58a89679d3c6e516d094cdde1e239579a6e373e3
mc36 <mc36@freemail.hu>
2022-01-20 06:44:37 +0100

    automatic commit at releng box

--- misc/tests/source-addresses.tcl
+++ misc/tests/source-addresses.tcl
@@ -3,4 +3,3 @@ for {set a 0} {$a <= 0x100} {incr a} {
   config "inter lo666" "ipv6 addr 2001:738:2001:db8::[string tohex w $a] /128"
   puts [exec "ping vpn.nop.hu /vrf inet2 /int lo666 /time 100"]
   }
-exec "config revert"
