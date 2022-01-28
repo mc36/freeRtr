112789237914b0b93e0465eeac29502d8b91d901
mc36 <mc36@freemail.hu>
2022-01-28 09:08:53 +0100

    automatic commit at releng box

--- src/net/freertr/tab/tabGenV2.java
+++ src/net/freertr/tab/tabGenV2.java
@@ -17,7 +17,8 @@ public class tabGenV2<T extends Comparator<? super T>> {
 
     private final static int thrsBlkD = 200;
 
-    private Object[][] valD = new Object[1][thrsElm]; // values
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private T[][] valD = (T[][]) new Comparator[1][thrsElm]; // values
 
     private int[] begD = new int[1]; // beginnings of blocks
 
@@ -45,6 +46,7 @@ public class tabGenV2<T extends Comparator<? super T>> {
      *
      * @param old where from copy
      */
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public tabGenV2(tabGenV2<T> old) {
         synchronized (old.lck) {
             begD = old.begD;
@@ -54,8 +56,8 @@ public class tabGenV2<T extends Comparator<? super T>> {
             blkN = old.blkN;
             doArrays(valD.length);
             for (int o = 0; o < blkN; o++) {
-                Object[] rowO = valD[o];
-                Object[] rowN = new Object[rowO.length];
+                T[] rowO = valD[o];
+                T[] rowN = (T[]) new Comparator[rowO.length];
                 bits.objCopy(rowO, 0, rowN, 0, sizD[o]);
                 valD[o] = rowN;
             }
@@ -65,9 +67,10 @@ public class tabGenV2<T extends Comparator<? super T>> {
     /**
      * optimize for lookup
      */
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public void optimize4lookup() {
         synchronized (lck) {
-            Object[] row = new Object[valN];
+            T[] row = (T[]) new Comparator[valN];
             int pos = 0;
             for (int o = 0; o < blkN; o++) {
                 bits.objCopy(valD[o], 0, row, pos, sizD[o]);
@@ -75,7 +78,7 @@ public class tabGenV2<T extends Comparator<? super T>> {
             }
             lstB = 0;
             blkN = 1;
-            valD = new Object[1][0];
+            valD = (T[][]) new Comparator[1][0];
             valD[0] = row;
             sizD[0] = valN;
             begD[0] = 0;
@@ -96,11 +99,12 @@ public class tabGenV2<T extends Comparator<? super T>> {
     /**
      * delete all values
      */
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public void clear() {
         synchronized (lck) {
             valN = 0;
             blkN = 1;
-            valD[0] = new Object[thrsElm];
+            valD[0] = (T[]) new Comparator[thrsElm];
             begD[0] = 0;
             sizD[0] = 0;
             lstB = 0;
@@ -213,14 +217,13 @@ public class tabGenV2<T extends Comparator<? super T>> {
         }
     }
 
-    @SuppressWarnings("unchecked")
     private int doFind(T val) {
         int lower = 0;
         int upper = blkN - 1;
         boolean hit = false;
         if (upper > 0) {
             if (lstB >= upper) {
-                int cmp = val.compare((T) valD[lstB][0], val);
+                int cmp = val.compare(valD[lstB][0], val);
                 if (cmp <= 0) {
                     lower = lstB + 1;
                     hit = true;
@@ -228,7 +231,7 @@ public class tabGenV2<T extends Comparator<? super T>> {
             }
             while (lower <= upper) {
                 lstB = (lower + upper) >>> 1;
-                int cmp = val.compare((T) valD[lstB][0], val);
+                int cmp = val.compare(valD[lstB][0], val);
                 if (cmp < 0) {
                     lower = lstB + 1;
                     continue;
@@ -250,9 +253,9 @@ public class tabGenV2<T extends Comparator<? super T>> {
         lower = 0;
         upper = sizD[lstB] - 1;
         int rowB = begD[lstB];
-        Object[] rowD = valD[lstB];
+        T[] rowD = valD[lstB];
         if (hit && (upper >= 0)) {
-            int cmp = val.compare((T) rowD[upper], val);
+            int cmp = val.compare(rowD[upper], val);
             if (cmp < 0) {
                 lstI = upper;
                 return -rowB - (upper + 1) - 1;
@@ -260,7 +263,7 @@ public class tabGenV2<T extends Comparator<? super T>> {
         }
         while (lower <= upper) {
             lstI = (lower + upper) >>> 1;
-            int cmp = val.compare((T) rowD[lstI], val);
+            int cmp = val.compare(rowD[lstI], val);
             if (cmp < 0) {
                 lower = lstI + 1;
                 continue;
@@ -306,20 +309,19 @@ public class tabGenV2<T extends Comparator<? super T>> {
         return 0;
     }
 
-    @SuppressWarnings("unchecked")
     private T doGet(int idx) {
         if (blkN <= 1) {
             lstI = idx;
-            return (T) valD[0][idx];
+            return valD[0][idx];
         }
         if (isCache(idx) != 0) {
             doCache(idx);
         }
-        return (T) valD[lstB][lstI];
+        return valD[lstB][lstI];
     }
 
     private void doDel() {
-        Object[] rowD = valD[lstB];
+        T[] rowD = valD[lstB];
         int siz = sizD[lstB] - 1;
         bits.objCopy(rowD, lstI + 1, rowD, lstI, siz - lstI);
         rowD[siz] = null;
@@ -346,12 +348,13 @@ public class tabGenV2<T extends Comparator<? super T>> {
         doArrays(blkN + thrsBlkA);
     }
 
+    @SuppressWarnings({"unchecked", "rawtypes"})
     private void doIns(int idx, T val) {
         if (isCache(idx) != 0) {
             doCache(idx);
         }
         int siz = sizD[lstB];
-        Object[] row = valD[lstB];
+        T[] row = valD[lstB];
         bits.objCopy(row, lstI, row, lstI + 1, siz - lstI);
         siz++;
         row[lstI] = val;
@@ -370,13 +373,13 @@ public class tabGenV2<T extends Comparator<? super T>> {
             sizD[i] = sizD[i - 1];
         }
         blkN++;
-        Object[] rowO = valD[lstB];
+        T[] rowO = valD[lstB];
         int mid = siz / 2;
-        Object[] rowN = new Object[rowO.length];
+        T[] rowN = (T[]) new Comparator[rowO.length];
         bits.objCopy(rowO, 0, rowN, 0, mid);
         valD[lstB] = rowN;
         sizD[lstB] = mid;
-        rowN = new Object[rowO.length];
+        rowN = (T[]) new Comparator[rowO.length];
         siz -= mid;
         bits.objCopy(rowO, mid, rowN, 0, siz);
         lstB++;
@@ -391,10 +394,11 @@ public class tabGenV2<T extends Comparator<? super T>> {
         }
     }
 
+    @SuppressWarnings({"unchecked", "rawtypes"})
     private void doArrays(int siz) {
         int[] begF = new int[siz];
         int[] sizF = new int[siz];
-        Object[][] valF = new Object[siz][];
+        T[][] valF = (T[][]) new Comparator[siz][];
         for (int i = 0; i < blkN; i++) {
             sizF[i] = sizD[i];
             valF[i] = valD[i];
