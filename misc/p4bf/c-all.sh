85c550344c51928592eebe566bdf219455c6ef43
mc36 <mc36@freemail.hu>
2022-01-31 18:44:59 +0100

    automatic commit at releng box

--- misc/p4bf/c-all.sh
+++ misc/p4bf/c-all.sh
@@ -25,16 +25,17 @@
 #qemu-img resize --shrink p4bf.img 5.2G
 #fallocate -d p4bf.img
 #
-cd /home/mc36/rare/p4src
+cd ~/rare/p4src
 export SDE=/home/mc36/bf-sde-9.7.1
 export SDE_INSTALL=$SDE/install
 $SDE/install/bin/bf-p4c -I. $@ -Xp4c="--disable-parse-depth-limit" bf_router.p4
+if [ -f bf_router.tofino/pipe/tofino.bin ] ; then echo "*** compilation successful ***" ; fi
 rm -rf $SDE/install/bf_router.tofino
 mv bf_router.tofino/bf_router.conf $SDE/install/share/p4/targets/tofino/
 mv bf_router.tofino $SDE/install/
 #
 #$SDE/tools/p4_build.sh -I. $@ ./bf_router.p4
 #cd $SDE/logs/p4-build/bf_router
-#sudo csplit make.log /p4c/ /p4c/
+#csplit make.log /p4c/ /p4c/
 #tail -n+2 xx01
-#sudo rm xx0*
+#rm xx0*
