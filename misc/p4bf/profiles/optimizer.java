9f1dbad53823d4b0267faf7e26fd9c5ca3ce658f
mc36 <mc36@freemail.hu>
2022-02-02 14:34:44 +0100

    automatic commit at releng box

--- misc/p4bf/profiles/optimizer.java
+++ misc/p4bf/profiles/optimizer.java
@@ -105,19 +105,41 @@ public class optimizer {
 
     private final static String tempFile = "optimizer.tmp";
 
-    private static List<String> doTransform(List<String> orig, int num) {
+    private final static String tempProg = "optimizer";
+
+    private static boolean checkString(List<String> orig, String str) {
+        for (int i = 0; i < orig.size(); i++) {
+            if (orig.get(i).indexOf(str) >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static List<String> doTransform(List<String> orig, int num1, int num2) {
         List<String> res = new ArrayList<String>();
         for (int i = 0; i < orig.size(); i++) {
             String a = orig.get(i);
-            int o = a.indexOf("$");
-            if (o < 10) {
+            int o = a.indexOf("$p");
+            int p = a.indexOf("$s");
+            if ((o + p) < 10) {
                 res.add(a);
                 continue;
+            }
+            if (o < 0) {
+                o = a.length();
+            }
+            if (p < 0) {
+                p = a.length();
+            }
+            if (p < o) {
+                o = p;
             }
             o -= 8;
             String b = a.substring(o, a.length());
             a = a.substring(0, o);
-            b = b.replaceAll("\\$", "" + num);
+            b = b.replaceAll("\\$p", "" + num1);
+            b = b.replaceAll("\\$s", "" + num2);
             doWrite(tempFile, "echo " + b + " | bc");
             b = doExec("bash " + tempFile).get(0);
             res.add(a + b);
@@ -127,29 +149,31 @@ public class optimizer {
         return res;
     }
 
-    private static boolean doRound(String prof, List<String> orig, int num) {
-        log("trying with " + num);
+    private static boolean doRound(List<String> orig, int num1, int num2) {
+        log("trying with " + num1 + " and " + num2);
         doDelete(tempFile);
-        List<String> res = doTransform(orig, num);
+        List<String> res = doTransform(orig, num1, num2);
         res.add("#include \"bf_router.p4\"");
-        doWrite(prof + ".p4", res);
-        doExec(System.getenv("SDE") + "/install/bin/bf-p4c -I../p4src/ -Xp4c=\"--disable-parse-depth-limit\" " + prof + ".p4");
-        boolean succ = new File(prof + ".tofino/pipe/tofino.bin").exists();
-        doWrite(tempFile, "rm -rf " + prof + ".tofino");
+        doWrite(tempProg + ".p4", res);
+        doExec(System.getenv("SDE") + "/install/bin/bf-p4c -I../p4src/ -Xp4c=\"--disable-parse-depth-limit\" " + tempProg + ".p4");
+        boolean succ = new File(tempProg + ".tofino/pipe/tofino.bin").exists();
+        doWrite(tempFile, "rm -rf " + tempProg + ".tofino");
         doExec("bash " + tempFile);
-        doDelete(prof + ".p4");
-        log("returning " + succ + " for " + num);
+        doDelete(tempProg + ".p4");
+        log("returning " + succ + " for " + num1 + " and " + num2);
         return succ;
     }
 
-    private static void doFinal(String prof, List<String> orig, int num) {
-        List<String> res = doTransform(orig, num);
+    private static void doFinal(String prof, List<String> orig, int num1, int num2) {
+        List<String> res = doTransform(orig, num1, num2);
         res.add(0, "#ifdef " + prof.toUpperCase().replaceAll("-", "_"));
         res.add("#endif");
         doWrite(prof + ".p4", res);
     }
 
-    private final static int stepMin = 1024;
+    private final static int step1 = 1024;
+
+    private final static int step2 = 4096;
 
     /**
      * the main
@@ -161,36 +185,71 @@ public class optimizer {
         prof = prof.substring(0, prof.lastIndexOf("."));
         List<String> orig = doRead(prof + ".tmpl");
         log("read " + orig.size() + " lines from " + prof);
-        int num = 256 * stepMin;
-        boolean res = false;
-        for (;;) {
-            if (num < stepMin) {
-                break;
+        int num1 = step1;
+        int num2 = step2;
+        boolean res;
+        if (!checkString(orig, "$p")) {
+            log("*** no first value to optimize ***");
+        } else {
+            num1 = 256 * step1;
+            res = false;
+            for (;;) {
+                if (num1 < step1) {
+                    break;
+                }
+                res = doRound(orig, num1, num2);
+                if (res) {
+                    break;
+                }
+                num1 >>>= 1;
             }
-            res = doRound(prof, orig, num);
-            if (res) {
-                break;
-            }
-            num >>>= 1;
-        }
-        if (!res) {
-            log("*** unable to find a working value ***");
-            return;
-        }
-        for (;;) {
-            num += stepMin;
-            res = doRound(prof, orig, num);
             if (!res) {
-                break;
+                log("*** unable to find a working value ***");
+                return;
             }
+            for (;;) {
+                num1 += step1;
+                res = doRound(orig, num1, num2);
+                if (!res) {
+                    break;
+                }
+            }
+            num1 -= step1;
         }
-        num -= stepMin;
-        res = doRound(prof, orig, num);
+        if (!checkString(orig, "$s")) {
+            log("*** no second value to optimize ***");
+        } else {
+            num2 = 64 * step2;
+            res = false;
+            for (;;) {
+                if (num2 < step2) {
+                    break;
+                }
+                res = doRound(orig, num1, num2);
+                if (res) {
+                    break;
+                }
+                num2 >>>= 1;
+            }
+            if (!res) {
+                log("*** unable to find a working value ***");
+                return;
+            }
+            for (;;) {
+                num2 += step2;
+                res = doRound(orig, num1, num2);
+                if (!res) {
+                    break;
+                }
+            }
+            num2 -= step2;
+        }
+        res = doRound(orig, num1, num2);
         if (!res) {
             log("*** profile failed at final verification ***");
             return;
         }
-        doFinal(prof, orig, num);
+        doFinal(prof, orig, num1, num2);
         log("*** working profile generated successfully ***");
     }
 
