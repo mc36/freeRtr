83d5dab1a1620edae58dc759ce578c9264c7f0bb
mc36 <mc36@freemail.hu>
2022-04-20 09:44:32 +0200

    automatic commit at releng box

--- misc/native/c.sh
+++ misc/native/c.sh
@@ -3,8 +3,8 @@ TR=../../binTmp
 UM=`uname -m`
 CC="tcc"                                #tcc
 CC="llc"                                #llc
-CC="clang"                              #clang
 CC="gcc"                                #gcc
+CC="clang"                              #clang
 
 MD="-O0 -g"                             #devel
 MD="-O3 -g"                             #debug
