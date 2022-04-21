30101a479dffd13b3e216707b604b97f7bd87467
mc36 <mc36@freemail.hu>
2022-04-21 12:59:24 +0200

    automatic commit at releng box

--- misc/p4lang/c.sh
+++ misc/p4lang/c.sh
@@ -3,7 +3,7 @@
 #wget https://download.opensuse.org/repositories/home:p4lang/Debian_11/Release.key
 #sudo apt-key add ./Release.key
 #sudo apt install p4lang-pi p4lang-p4c p4lang-bmv2 psmisc gpg iproute2 net-tools tshark iperf gcc git telnet
-#git clone git@github.com:frederic-loui/RARE.git
+#git clone git@github.com:rare-freertr/RARE-bmv2
 #gcc -O3 -o dummyCon.bin ../native/dummyCon.c
 #cp initd /etc/init.d/rtr
 #chmod 755 /etc/init.d/rtr
