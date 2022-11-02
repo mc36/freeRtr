9b0b6c79e00e6d945a59360283587589d80ae087
mc36 <github@nop.hu>
2022-11-02 06:40:50 +0100

    automatic commit at releng box

--- misc/p4bf/f.sh
+++ misc/p4bf/f.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ###sleep 15
 cd /home/mc36/rare/bfrt_python
-export SDE=/home/mc36/bf-sde-9.10.0
+export SDE=/home/mc36/bf-sde-9.11.0
 export SDE_INSTALL=$SDE/install
 while (true); do
   python3 bf_forwarder.py --freerouter-address 10.10.10.227
