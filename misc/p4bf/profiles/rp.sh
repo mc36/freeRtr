288bb9499899f38d7126aeb9bdb35d84b77a340b
mc36 <github@nop.hu>
2023-01-05 16:41:53 +0100

    automatic commit at releng box

--- misc/p4bf/profiles/rp.sh
+++ misc/p4bf/profiles/rp.sh
@@ -5,6 +5,7 @@ set -o pipefail
 PATH=$PATH:$SDE_INSTALL/bin
 ncpus=$(lscpu -J | jq -r '.lscpu[] | select(.field == "CPU(s):") | .data')
 sde_version=$(p4c --version | awk '{print $2}')
+rc=0
 
 cleanup () {
     git restore rare_profiles.p4
@@ -13,13 +14,17 @@ cleanup () {
 trap cleanup INT TERM EXIT
 
 optimize_one () {
-    local profile=$(basename --suffix .tmpl $1)
+    local profile=$(basename --suffix .tmpl $1) rc=0
     echo "Starting optimization for $profile"
-    java optimizer $1 $2 >${profile}.log && cp $profile.p4 $3
+    if ! java optimizer $1 $2 >${profile}.log; then
+	cp $profile.p4 $3
+	rc=1
+    fi
     echo -n "Optimization for $profile finished: "
     tail -1 ${profile}.log
     rm $2
     git restore $profile.p4
+    return $rc
 }
 
 declare -A target_flags
@@ -32,7 +37,7 @@ optimize_target () {
     dir=$sde_version/$target
     echo "Optimizing for SDE $sde_version, target $target"
     mkdir -p $dir
-    for fn in profile-*.tmpl ; do
+    for fn in profile-*.tmpl; do
 	profile=$(echo $fn | sed -e 's/.*profile-\(.*\)\.tmpl/\1/' | tr [:lower:] [:upper:] | tr - _)
 	file=optimizer_$profile.txt
 	nix eval --extra-experimental-features nix-command --json --impure --expr \
@@ -42,11 +47,14 @@ optimize_target () {
 	optimize_one $fn $file $dir &
 	nprocs=$(($nprocs+1))
 	if [ $nprocs -eq $ncpus ]; then
-	    wait -n
+	    wait -n || rc=1
 	    nprocs=$(($nprocs-1))
 	fi
     done
-    wait
+    while [ $nprocs -gt 0 ]; do
+	wait -n || rc=1
+	nprocs=$(($nprocs-1))
+    done
     echo "##undef _TABLE_SIZE_P4_" > $dir/rare_profiles.p4
     for fn in profile-*.p4 ; do
 	echo "#include \"$fn\"" >> $dir/rare_profiles.p4
@@ -57,3 +65,4 @@ echo "" >rare_profiles.p4
 for target in tofino tofino2; do
     optimize_target $target
 done
+exit $rc
