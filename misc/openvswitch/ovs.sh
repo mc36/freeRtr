8bf810affcf049dbd2a4a584d18bc389c1c29549
mc36 <github@nop.hu>
2023-01-21 06:27:32 +0100

    automatic commit at releng box

--- misc/openvswitch/ovs.sh
+++ misc/openvswitch/ovs.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sleep 5
 ip link set ens4 up promisc on
 ip link set ens5 up promisc on
 ip link set ens6 up promisc on
 ip link set ens7 up promisc on
 ip link set ens8 up promisc on
-sleep 5
+sleep 1
 ovs-vsctl init
 ovs-vsctl del-br sw
+sleep 1
 ovs-vsctl add-br sw
-ovs-vsctl set-controller sw tcp:10.10.10.227:6653
+ovs-vsctl set-controller sw tcp:10.10.10.227
 ovs-vsctl add-port sw ens4
 ovs-vsctl add-port sw ens5
 ovs-vsctl add-port sw ens6
