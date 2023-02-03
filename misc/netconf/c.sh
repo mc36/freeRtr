e03476475c10c2582e08d95ae4eaf820aa547ca8
mc36 <github@nop.hu>
2023-02-03 15:36:35 +0100

    automatic commit at releng box

--- misc/netconf/c.sh
+++ misc/netconf/c.sh
@@ -2,3 +2,4 @@
 for fn in *.txt ; do
     java -jar ../../src/rtr.jar test yangconfig $fn ../../binTmp/
     done
+rm reload.log
--- misc/sensor/c.sh
+++ misc/sensor/c.sh
@@ -2,3 +2,4 @@
 for fn in *.txt ; do
     java -jar ../../src/rtr.jar test yangsensor $fn ../../binTmp/
     done
+rm reload.log
