8fc6833497e8af0b9687f29d54aa7389527fe6ca
mc36 <github@nop.hu>
2023-03-17 17:16:49 +0100

    automatic commit at releng box

--- misc/trackmap/lsabr.tcl
+++ misc/trackmap/lsabr.tcl
@@ -7,6 +7,6 @@ puts "<table border=1>$res</table>"
 
 puts "<html><head><title>lsa</title></head>"
 puts "<body bgcolor=black text=white>"
-dosh "show ipv4 lsrp 1 bier"
-dosh "show ipv6 lsrp 1 bier"
+dosh "show ipv4 bier inet"
+dosh "show ipv6 bier inet"
 puts "</body></html>"
--- misc/trackmap/lsasr.tcl
+++ misc/trackmap/lsasr.tcl
@@ -7,6 +7,6 @@ puts "<table border=1>$res</table>"
 
 puts "<html><head><title>lsa</title></head>"
 puts "<body bgcolor=black text=white>"
-dosh "show ipv4 lsrp 1 segrout"
-dosh "show ipv6 lsrp 1 segrout"
+dosh "show ipv4 segrout inet"
+dosh "show ipv6 segrout inet"
 puts "</body></html>"
