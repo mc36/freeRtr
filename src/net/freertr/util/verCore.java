42a81826dd6a3db6e7b378615b871f619204f4f9
mc36 <github@nop.hu>
2023-05-12 15:53:07 +0200

    automatic commit at releng box

--- src/net/freertr/util/verCore.java
+++ src/net/freertr/util/verCore.java
@@ -66,7 +66,8 @@ public class verCore {
     public final static String[] license = {
         "place on the web: " + homeUrl,
         "license: http://creativecommons.org/licenses/by-sa/4.0/",
-        "quote-2: true random comes from weather forecasts and political announcements",
+        "quote-3: true random comes from weather forecasts and political announcements",
+        "quote-2: stupid question deserves stupid answers",
         "quote-1: use your brain",
         "quote0: know your shit",
         "quote1: do or not to do but don't try",
