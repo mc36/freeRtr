19d1e51d118416268a2ca1d00245da768ffb61fa
mc36 <github@nop.hu>
2023-05-16 03:48:34 +0200

    automatic commit at releng box
    todo: client upgrade-simulate url
    todo: client upgrade-force url
    todo: flash overlap-clear

--- src/tp9.sh
+++ src/tp9.sh
@@ -2,8 +2,6 @@
 cd ../misc/native
 ./c.sh
 cd ../../src
-./c.sh
-./cb.sh
 java -Xmx512m -jar rtr.jar test tester p4lang- binary other p4lang9.ini summary slot 1 paralell 60 retry 4 url http://sources.freertr.org/cfg/ $@
 java -Xmx512m -jar rtr.jar test tstsum
 ./te.sh
