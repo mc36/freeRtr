586495c5f0a8f5e55be0b1dff0a920375b3ede1a
mc36 <github@nop.hu>
2023-07-18 09:04:35 +0200

    automatic commit at releng box

---
+++ misc/gallery/staticer.java
@@ -0,0 +1,18 @@
+
+/**
+ * static image gallery updater
+ *
+ * @author matecsaba
+ */
+public class staticer {
+
+    /**
+     * this is needed for cli startup
+     *
+     * @param args command line parameters
+     */
+    public static void main(String[] args) {
+        //////////////
+    }
+
+}
