1cd1a22441e096afb6458c2bca81ff3dedfe704c
mc36 <github@nop.hu>
2023-11-11 22:16:01 +0100

    automatic commit at releng box

--- misc/image/cj.sh
+++ misc/image/cj.sh
@@ -3,3 +3,4 @@ cd ../../src
 java -Xmx256m -jar rtr.jar test image ../misc/image/image.clr
 java -Xmx256m -jar rtr.jar test image ../misc/image/plat.amd64 ../misc/image/image.jvm
 java -Xmx256m -jar rtr.jar test image ../misc/image/plat.i686 ../misc/image/image.jvm
+java -Xmx256m -jar rtr.jar test image ../misc/image/plat.arm64 ../misc/image/image.jvm
