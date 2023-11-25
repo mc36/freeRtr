2dd381c64cfc8eca292a6aebcd15b84e50c87d56
mc36 <github@nop.hu>
2023-11-25 02:08:26 +0100

    automatic commit at releng box

--- misc/p4bf/rare/bf_gbl_env/var_env.py
+++ misc/p4bf/rare/bf_gbl_env/var_env.py
@@ -14,7 +14,7 @@ SDE_INSTALL = os.environ.get("SDE_INSTALL", SDE + "/install")
 BF_RUNTIME_LIB = SDE_INSTALL + "/lib/python3.9/site-packages/tofino/"
 BSP_FILE_PATH = SDE_INSTALL + "/lib/libpltfm_mgr.so"
 
-sys.path.insert(0, SDE_INSTALL + "/lib/python3.9/site-packages/")
+sys.path.append(SDE_INSTALL + "/lib/python3.9/site-packages/")
 import grpc
 
 # set our lib path
