500309af2094796f1f4b74aff45bfb3a420fdf6d
mc36 <github@nop.hu>
2023-11-30 08:51:31 +0100

    automatic commit at releng box

--- src/cb.sh
+++ src/cb.sh
@@ -3,5 +3,5 @@ echo compiling
 cp rtr.jar rtr2.jar
 zip -d rtr2.jar "net/freertr/pipe/pipeWindow*"
 zip -d rtr2.jar "net/freertr/user/userGame**"
-native-image -O3 --no-fallback -jar rtr2.jar rtr.bin
+native-image -O3 --no-fallback -jar rtr2.jar -o rtr.bin
 upx -o rtr2.bin rtr.bin
