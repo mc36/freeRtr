691a6fecb97594116e43f66c98348c8b5ea24dd2
mc36 <github@nop.hu>
2024-01-26 17:50:12 +0100

    automatic commit at releng box

--- misc/image/cc.sh
+++ misc/image/cc.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd ../../src
 java -Xmx256m -jar rtr.jar test image ../misc/image/image.clr
-for a in amd64 i686 arm64 arm32 mips64 ppc64 risc64 s390x sparc sh4 m68k ; do
+for a in arm64 arm32 mips64 ppc64 risc64 s390x sparc sh4 m68k hppa ; do
   java -Xmx256m -jar rtr.jar test image ../misc/image/plat.$a ../misc/image/image.cmp
+  bash ../binDsk/r.sh
+  mv ../binDsk/binImg/*.tar ../binImg/
   done
