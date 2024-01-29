799f724a3a4f7535c695e69c8722f62959ab2062
mc36 <github@nop.hu>
2024-01-29 08:58:38 +0100

    automatic commit at releng box

--- misc/native/p4emu_bench.c
+++ misc/native/p4emu_bench.c
@@ -77,6 +77,7 @@ int main(int argc, char **argv) {
         free(lin);
     }
     fclose(fil);
+    sleep(1);
     clock_t begin = clock();
     for (int i = 0; i < count; i++) {
         memcpy(&bufD[preBuff], &origD[0], origS);
