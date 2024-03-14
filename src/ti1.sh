b9ee16240f7766a15d747a43b6b4b508e1b8b0fc
mc36 <github@nop.hu>
2024-03-14 17:28:56 +0100

    automatic commit at releng box

--- src/ti1.sh
+++ src/ti1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-java -Xmx512m -jar rtr.jar test tester intop1- persist intop1.ini discard .*ike.* summary slot 101 retry 8 url http://sources.freertr.org/cfg/ randord $@
+java -Xmx512m -jar rtr.jar test tester intop1- persist intop1.ini summary slot 101 retry 8 url http://sources.freertr.org/cfg/ randord $@
 ./te.sh
 ./tm.sh rtrintop1- rtr9
