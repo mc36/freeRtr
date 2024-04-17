8b6df9c5fe06a651b59f11ffdf442c6ade3062f4
mc36 <github@nop.hu>
2024-04-17 20:22:43 +0200

    automatic commit at releng box

--- misc/image/ca.sh
+++ misc/image/ca.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../../src
-for a in amd64 i686 arm64 arm32 mips64 ppc64 risc64 s390x sparc sh4 ; do
+for a in amd64 i686 arm64 arm32 mips64 ppc64 risc64 s390x loong sparc sh4 ; do
   java -Xmx256m -jar rtr.jar test image ../misc/image/plat.$a ../misc/image/image.jvm
   done
