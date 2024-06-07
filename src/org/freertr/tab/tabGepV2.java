3ada4fc8a2db264bc5501352aaaaa4deb9485dcd
mc36 <github@nop.hu>
2024-06-07 10:31:51 +0200

    automatic commit at releng box

--- src/org/freertr/tab/tabGepV2.java
+++ src/org/freertr/tab/tabGepV2.java
@@ -17,6 +17,7 @@ public class tabGepV2<T extends addrType> {
      */
     public tabGepV2() {
         root = new tabGepV2nod<T>();
+        cache(root);
     }
 
     /**
@@ -26,10 +27,16 @@ public class tabGepV2<T extends addrType> {
      */
     public void add(tabRouteEntry<T> val) {
         tabGepV2nod<T> cur = root;
+        tabGepV2nod<T> bas = root;
         for (int p = 0;; p++) {
             if (p >= val.prefix.maskLen) {
                 cur.val = val;
+                cache(bas);
                 return;
+            }
+            if ((p & 7) == 0) {
+                cache(bas);
+                bas = cur;
             }
             if (val.prefix.network.bitValue(p)) {
                 if (cur.one == null) {
@@ -52,10 +59,15 @@ public class tabGepV2<T extends addrType> {
      */
     public void del(tabRouteEntry<T> val) {
         tabGepV2nod<T> cur = root;
+        tabGepV2nod<T> bas = root;
         for (int p = 0;; p++) {
             if (p >= val.prefix.maskLen) {
                 cur.val = null;
+                cache(bas);
                 return;
+            }
+            if ((p & 7) == 0) {
+                bas = cur;
             }
             if (val.prefix.network.bitValue(p)) {
                 cur = cur.one;
@@ -68,13 +80,35 @@ public class tabGepV2<T extends addrType> {
         }
     }
 
-    /**
-     * find one container entry
-     *
-     * @param val value to look up
-     * @return found value
-     */
-    public tabRouteEntry<T> search(tabRouteEntry<T> val) {
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private void cache(tabGepV2nod<T> bas) {
+        tabGepV2nod<T> res[] = new tabGepV2nod[256];
+        cache(res, bas, null, 0, res.length);
+        bas.cache = res;
+    }
+
+    private void cache(tabGepV2nod<T> res[], tabGepV2nod<T> cur, tabRouteEntry<T> lst, int beg, int end) {
+        if (cur == null) {
+            return;
+        }
+        if (cur.val != null) {
+            lst = cur.val;
+        }
+        cur.result = lst;
+        for (int i = beg; i < end; i++) {
+            res[i] = cur;
+        }
+        int mid = end - beg;
+        if (mid <= 1) {
+            return;
+        }
+        mid >>>= 1;
+        mid += beg;
+        cache(res, cur.zero, lst, beg, mid);
+        cache(res, cur.one, lst, mid, end);
+    }
+
+    private tabRouteEntry<T> search_uncached(tabRouteEntry<T> val) {
         tabGepV2nod<T> cur = root;
         tabRouteEntry<T> lst = null;
         for (int p = 0;; p++) {
@@ -95,6 +129,33 @@ public class tabGepV2<T extends addrType> {
         }
     }
 
+    /**
+     * find one container entry
+     *
+     * @param val value to look up
+     * @return found value
+     */
+    public tabRouteEntry<T> search(tabRouteEntry<T> val) {
+        tabGepV2nod<T> cur = root;
+        tabRouteEntry<T> lst = null;
+        for (int p = 0;; p += 8) {
+            if (cur.result != null) {
+                lst = cur.result;
+            }
+            if (cur.cache == null) {
+                return lst;
+            }
+            if (p >= val.prefix.maskLen) {
+                return lst;
+            }
+            int i = val.prefix.network.getBytes()[p >>> 3] & 0xff;
+            cur = cur.cache[i];
+            if (cur == null) {
+                return lst;
+            }
+        }
+    }
+
 }
 
 class tabGepV2nod<T extends addrType> {
@@ -104,6 +165,10 @@ class tabGepV2nod<T extends addrType> {
     public tabGepV2nod<T> one;
 
     public tabRouteEntry<T> val;
+
+    public tabRouteEntry<T> result;
+
+    public tabGepV2nod<T> cache[];
 
     public tabGepV2nod() {
         val = null;
