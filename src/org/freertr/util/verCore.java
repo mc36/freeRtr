c4a28ebd54c1587e470ed1a2a6975e8e14d7c855
mc36 <github@nop.hu>
2024-08-01 05:55:02 +0200

    automatic commit at releng box

--- src/org/freertr/util/verCore.java
+++ src/org/freertr/util/verCore.java
@@ -43,12 +43,12 @@ public class verCore {
     /**
      * compile month
      */
-    public final static int month = 7;
+    public final static int month = 8;
 
     /**
      * compile day
      */
-    public final static int day = 31;
+    public final static int day = 1;
 
     /**
      * statement of release
