d1266171057e0acf94c426bf509ff904579fdf53
mc36 <github@nop.hu>
2024-12-30 17:14:51 +0100

    automatic commit at releng box

--- misc/player/receiver.java
+++ misc/player/receiver.java
@@ -1,7 +1,9 @@
 
-import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.SourceDataLine;
@@ -9,18 +11,24 @@ import javax.sound.sampled.SourceDataLine;
 public class receiver {
 
     public static void main(String[] args) throws Exception {
+        InetAddress group = InetAddress.getByName(args[0]);
+        InetAddress source = InetAddress.getByName(args[1]);
+        int port = Integer.parseInt(args[2]);
+
         AudioFormat audioFormat = new AudioFormat(44100, 16, 2, true, true);
         SourceDataLine sourceDataLine = AudioSystem.getSourceDataLine(audioFormat);
         sourceDataLine.open(audioFormat);
         sourceDataLine.start();
 
-        MulticastSocket socket = new MulticastSocket(1234);
-        socket.joinGroup(InetAddress.getByName("232.2.3.2"));
-        byte[] buffer = new byte[65536];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        MulticastSocket mcstsck = new MulticastSocket();
+        DatagramChannel channel = DatagramChannel.open();
+        channel.socket().bind(new InetSocketAddress(port));
+        channel.join(group, mcstsck.getNetworkInterface(), source);
+        ByteBuffer buffer = ByteBuffer.allocate(4096);
         for (;;) {
-            socket.receive(packet);
-            sourceDataLine.write(buffer, 12, packet.getLength() - 12);
+            buffer.clear();
+            channel.receive(buffer);
+            sourceDataLine.write(buffer.array(), 12, buffer.position() - 12);
         }
     }
 
