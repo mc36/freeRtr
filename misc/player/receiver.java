cc50a00fc6b0d18177a62d96a65f8c771f06e81f
mc36 <github@nop.hu>
2024-12-31 08:31:20 +0100

    automatic commit at releng box

--- misc/player/receiver.java
+++ misc/player/receiver.java
@@ -6,17 +6,26 @@ import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Mixer;
 import javax.sound.sampled.SourceDataLine;
 
 public class receiver {
 
     public static void main(String[] args) throws Exception {
-        InetAddress group = InetAddress.getByName(args[0]);
-        InetAddress source = InetAddress.getByName(args[1]);
-        int port = Integer.parseInt(args[2]);
+        Mixer.Info[] mixers = AudioSystem.getMixerInfo();
+        int mixerc = -1;
+        for (int i = 0; i < mixers.length; i++) {
+            if (mixers[i].getName().contains(args[0])) {
+                mixerc = i;
+            }
+            System.out.println(mixers[i].getName() + " - " + mixers[i].getDescription());
+        }
+        InetAddress group = InetAddress.getByName(args[1]);
+        InetAddress source = InetAddress.getByName(args[2]);
+        int port = Integer.parseInt(args[3]);
 
         AudioFormat audioFormat = new AudioFormat(44100, 16, 2, true, true);
-        SourceDataLine sourceDataLine = AudioSystem.getSourceDataLine(audioFormat);
+        SourceDataLine sourceDataLine = AudioSystem.getSourceDataLine(audioFormat, mixers[mixerc]);
         sourceDataLine.open(audioFormat);
         sourceDataLine.start();
 
