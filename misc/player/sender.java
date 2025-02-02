0ba8361a8361c48a27fc0f4c0487e07a62ec3c8e
mc36 <github@nop.hu>
2025-02-02 08:41:14 +0100

    automatic commit at releng box

--- misc/player/sender.java
+++ misc/player/sender.java
@@ -29,6 +29,7 @@ public class sender {
 
         int seq = 0;
         int clk = 0;
+        int src = (int) process.pid();
         for (;;) {
             byte[] buf = new byte[1024];
             int i = stream.read(buf, 0, buf.length);
@@ -42,21 +43,24 @@ public class sender {
                 continue;
             }
             buffer.clear();
-            buffer.put(0, (byte) 0x80);
-            buffer.put(1, (byte) 0xa);
-            buffer.put(2, (byte) (seq >>> 8));
-            buffer.put(3, (byte) seq);
-            buffer.put(4, (byte) (clk >>> 24));
-            buffer.put(5, (byte) (clk >>> 16));
-            buffer.put(6, (byte) (clk >>> 8));
-            buffer.put(7, (byte) clk);
+            putMsb(buffer, 0, 0x800a0000 | seq);
+            putMsb(buffer, 4, clk);
+            putMsb(buffer, 8, src);
             buffer.put(12, buf, 0, i);
             buffer.position(0);
             buffer.limit(i + 12);
             channel.write(buffer);
             seq++;
+            seq &= 0xffff;
             clk += i / 4;
         }
+    }
+
+    private static void putMsb(ByteBuffer buf, int ofs, int val) {
+        buf.put(ofs + 0, (byte) (val >>> 24));
+        buf.put(ofs + 1, (byte) (val >>> 16));
+        buf.put(ofs + 2, (byte) (val >>> 8));
+        buf.put(ofs + 3, (byte) val);
     }
 
 }
