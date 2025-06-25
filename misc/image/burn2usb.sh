6f64c773174dc33856bf183091c155fe887eaec7
mc36 <github@nop.hu>
2025-06-25 13:37:20 +0200

    automatic commit at releng box

--- misc/image/burn2usb.sh
+++ misc/image/burn2usb.sh
@@ -4,7 +4,7 @@ sudo mkfs.vfat /dev/sdb1
 sudo lilo -S /dev/null -M /dev/sdb
 sudo syslinux /dev/sdb1
 sudo mount -t vfat -o rw /dev/sdb1 /mnt
-sudo cp boot.cfg /mnt/syslinux.cfg
+sudo cp boot.sys /mnt/syslinux.cfg
 sudo cp ../../binImg/rtr-x86_64.krn /mnt/rtr.krn
 sudo cp ../../binImg/rtr-x86_64.ird /mnt/rtr.ird
 sudo umount /mnt
