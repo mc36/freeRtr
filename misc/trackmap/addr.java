5efca559c6badd7f1ce6612d606713fc1b3c65ce
mc36 <github@nop.hu>
2025-10-07 00:29:55 +0200

    automatic commit at releng box

--- misc/trackmap/addr.java
+++ misc/trackmap/addr.java
@@ -1,5 +1,6 @@
 
 import java.io.ByteArrayOutputStream;
+import java.io.RandomAccessFile;
 
 /**
  * sample http applet
@@ -28,6 +29,8 @@ public class addr {
         System.out.println(a);
     }
 
+    private static String style = null;
+
     /**
      * do one request
      *
@@ -44,7 +47,18 @@ public class addr {
     public String httpRequest(String url, String path, String peer,
             String agent, String user, String[] par, ByteArrayOutputStream buf)
             throws Exception {
-        String s = "<!DOCTYPE html><html lang=\"en\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"addr.css\" /><title>address</title></head><body>" + peer + "</body></html>";
+        if (style == null) {
+            try {
+                RandomAccessFile fr = new RandomAccessFile(path.substring(0, path.lastIndexOf(".")) + ".css", "r");
+                byte[] red = new byte[(int) fr.length()];
+                fr.read(red, 0, red.length);
+                fr.close();
+                style = new String(red);
+            } catch (Exception e) {
+                style = "";
+            }
+        }
+        String s = "<!DOCTYPE html><html lang=\"en\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /><style>\n" + style + "</style><title>address</title></head><body>\n" + peer + "\n</body></html>\n";
         buf.write(s.getBytes());
         return "html";
     }
