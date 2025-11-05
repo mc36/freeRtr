3c961ab18c73d8b601fcf95d6e5931d6f48833cf
mc36 <github@nop.hu>
2025-11-05 10:10:54 +0100

    automatic commit at releng box

--- misc/image/ca.sh
+++ misc/image/ca.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ../../src
-for a in amd64 i686 arm64 arm32hf arm32el alpha mips64 ppc32 ppc64el risc64 s390x loong sparc sh4 ; do
+for a in amd64 i686 arm64 arm32hf alpha ppc32 ppc64el risc64 s390x loong sparc sh4 ; do
   java -Xmx256m -jar rtr.jar test image ../misc/image/platform.$a ../misc/image/image.jvm
   done
 java -Xmx256m -jar rtr.jar test image ../misc/image/platform.amd64 ../misc/image/image.fbsd
--- misc/image/cc.sh
+++ misc/image/cc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../../src
-for a in i686 amd64 arm32hf arm32el mips64 alpha ppc32 ppc64el ppc64eb risc64 s390x sparc loong sh4 m68k hppa arm64 ; do
+for a in i686 amd64 arm32hf alpha ppc32 ppc64el ppc64eb risc64 s390x sparc loong sh4 m68k hppa arm64 ; do
   java -Xmx256m -jar rtr.jar test image ../misc/image/platform.$a ../misc/image/native.cross
   done
