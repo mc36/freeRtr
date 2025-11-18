31fa7f935eaa372c61f93c36da18d5c208d60264
mc36 <github@nop.hu>
2025-11-18 12:29:03 +0100

    automatic commit at releng box

--- misc/native/p4emu_fuzzer.sh
+++ misc/native/p4emu_fuzzer.sh
@@ -4,7 +4,7 @@ CO=../../binFuz/
 FN=../../binTmp/p4fuzz.tmp
 mkdir -p $CO
 clang -fsanitize=fuzzer -O -lcrypto -o $FN p4emu_fuzzer.c
-$FN --p4emu_bench_cmds.txt -max_len=1500 -max_total_time=180 $CO
+$FN --p4emu_bench_cmds.txt -max_len=1500 -max_total_time=60 $CO
 rm $FN
 find $CO -type f -mtime +7 -delete
 find $CO -type f -size -14c -delete
