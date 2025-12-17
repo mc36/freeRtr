1c2d1c348fbb402fc55313eb2692cdda458589e3
mc36 <github@nop.hu>
2025-12-17 01:59:06 +0100

    automatic commit at releng box
    qc pass: p4lang: monitor session
    qc pass: p4lang: flow inspection

--- src/tp7.sh
+++ src/tp7.sh
@@ -4,4 +4,5 @@ ulimit -n 16384
 export p4emuNOCMDS=1
 export p4emuNOCONS=1
 java -Xmx512m -jar rtr.jar test tester p4lang- binary other p4lang7.ini slot 1 parallel 30 retry 4 $@
+killall -9 memcheck-amd64-linux
 ./te.sh
