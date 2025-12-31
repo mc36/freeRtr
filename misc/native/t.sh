5f96814d206267da5e0370791a31ba1c42e67ef3
mc36 <github@nop.hu>
2025-12-31 13:56:38 +0100

    automatic commit at releng box

--- misc/native/t.sh
+++ misc/native/t.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 clang-tidy -header-filter=.* --checks=-clang-analyzer-security.insecureAPI.strcpy,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling,-clang-analyzer-security.insecureAPI.UncheckedReturn p4emu_full.c
+cppcheck --suppress=nullPointerOutOfMemory p4emu_full.c
