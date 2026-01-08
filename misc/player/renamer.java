8f26d567bff6a27578d6b81801f089f6e6173c08
mc36 <github@nop.hu>
2026-01-08 23:58:23 +0100

    automatic commit at releng box

--- misc/player/renamer.java
+++ misc/player/renamer.java
@@ -153,6 +153,7 @@ public class renamer {
         s = s.replaceAll("？", "");
         s = s.replaceAll("…", "");
         s = s.replaceAll("”", "");
+        s = s.replaceAll("´", "");
         s = s.replaceAll("“", "");
         s = s.replaceAll("’", "");
         s = s.replaceAll("=", " ");
