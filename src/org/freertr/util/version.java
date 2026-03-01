2f86cf114edf0a1719202a5865ad03184c2e851c
mc36 <github@nop.hu>
2026-03-01 06:09:02 +0100

    automatic commit at releng box

--- src/org/freertr/util/version.java
+++ src/org/freertr/util/version.java
@@ -38,12 +38,12 @@ public class version {
     /**
      * compile month
      */
-    public final static int month = 2;
+    public final static int month = 3;
 
     /**
      * compile day
      */
-    public final static int day = 28;
+    public final static int day = 1;
 
     /**
      * statement of release
