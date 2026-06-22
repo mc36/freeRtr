c8f6dd14f713b8cf87faa5727625102aa51759c7
mc36 <github@nop.hu>
2026-06-22 21:18:21 +0200

    automatic commit at releng box

--- misc/streamer/playback.java
+++ misc/streamer/playback.java
@@ -4,8 +4,8 @@ import javax.sound.sampled.SourceDataLine;
 public class playback {
 
     public static void main(String[] args) throws Exception {
-        decoder dec = new decoder(args[1], args[2]);
-        SourceDataLine dataLine = devicer.getPlayback(args[0]);
+        decoder dec = new decoder(args[0], args[1]);
+        SourceDataLine dataLine = devicer.getPlayback(args[2]);
         for (;;) {
             byte[] buf = new byte[rtper.payload];
             int i = dec.read(buf);
