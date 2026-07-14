037ffd357338caeb5528072eb6525432f8a3b5bb
mc36 <github@nop.hu>
2026-07-14 08:25:47 +0200

    automatic commit at releng box

--- misc/streamer/alsacap.sh
+++ misc/streamer/alsacap.sh
@@ -2,4 +2,5 @@
 gcc -o alsacap.bin alsacap.c -lasound
 #wget https://raw.githubusercontent.com/bubbapizza/alsacap/master/src/alsacap.c
 #wget https://raw.githubusercontent.com/RishabhHardas/ALSA-wav-player/refs/heads/master/wav_player.c
-#apt-get install libasound2-dev alsa-utils
+#wget https://raw.githubusercontent.com/kyx0r/nextfvp/refs/heads/main/fvp.c
+#apt-get install libasound2-dev libavutil-dev libavcodec-dev libavformat-dev libavfilter-dev alsa-utils
