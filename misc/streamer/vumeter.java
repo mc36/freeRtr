26f3935c053365e668eeef486560613632fb5073
mc36 <github@nop.hu>
2026-07-23 07:53:06 +0200

    automatic commit at releng box

---
+++ misc/streamer/vumeter.java
@@ -0,0 +1,103 @@
+
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+/**
+ * measure live level
+ *
+ * @author matecsaba
+ */
+public class vumeter {
+
+    private static double getSam(byte[] buf, int ofs) {
+        int i = (buf[ofs + 0] << 8) + (buf[ofs + 1] & 0xff);
+        return (double) i / 32768.0;
+    }
+
+    private static double getVu(double sum, int len) {
+        double rms = Math.sqrt((double) sum * 4.0 / (double) len);
+        return (20.0 * Math.log10(rms)) + 3.8;
+    }
+
+    private static double getAng(double vu) {
+        if (vu <= -20) {
+            return -25;
+        }
+        if (vu >= 3) {
+            return 25;
+        }
+        if (vu <= -20) {
+            return -23;
+        }
+        if (vu <= -10) {
+            return -23 + ((vu + 20) / 10) * 7;
+        }
+        if (vu <= -7) {
+            return -16 + ((vu + 10) / 3) * 4;
+        }
+        if (vu <= -5) {
+            return -12 + ((vu + 7) / 2) * 4;
+        }
+        if (vu <= -3) {
+            return -8 + ((vu + 5) / 2) * 5;
+        }
+        if (vu <= -2) {
+            return -3 + ((vu + 3) / 1) * 3;
+        }
+        if (vu <= -1) {
+            return 0 + ((vu + 2) / 1) * 3.5;
+        }
+        if (vu <= 0) {
+            return 3.5 + ((vu + 1) / 1) * 4.5;
+        }
+        if (vu <= 1) {
+            return 8 + (vu / 1) * 5;
+        }
+        if (vu <= 2) {
+            return 13 + ((vu - 1) / 1) * 5;
+        }
+        if (vu <= 3) {
+            return 18 + ((vu - 2) / 1) * 7;
+        } else {
+            return 25;
+        }
+    }
+
+    private static String vuBar(double val) {
+        String a = "";
+        int cur = 25 + (int) val;
+        for (int i = 0; i < cur; i++) {
+            a += "#";
+        }
+        for (int i = cur; i < 50; i++) {
+            a += " ";
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws Exception {
+        DatagramChannel channel = rtper.receive(args[0], args[1], args[2]);
+        ByteBuffer buffer = ByteBuffer.allocate(4096);
+        byte[] buf = new byte[rtper.payload];
+        for (;;) {
+            buffer.clear();
+            channel.receive(buffer);
+            int len = rtper.decode(buffer, buf);
+            if (len < 1) {
+                break;
+            }
+            double sumL = 0;
+            double sumR = 0;
+            for (int i = 0; i < len; i += 4) {
+                double o = getSam(buf, i + 0);
+                sumL += o * o;
+                o = getSam(buf, i + 2);
+                sumR += o * o;
+            }
+            sumL = getAng(getVu(sumL, len));
+            sumR = getAng(getVu(sumR, len));
+            System.out.print(vuBar(sumL) + " " + vuBar(sumR) + "\r");
+        }
+    }
+
+}
