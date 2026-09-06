f101f8d254028d5c89f400c9fdb1ce2730bbfc38
mc36 <github@nop.hu>
2026-09-06 04:21:39 +0200

    automatic commit at releng box
    no qc pass: dhcp with excluded address
    failed: dhcp with excluded address

--- misc/streamer/cn.sh
+++ misc/streamer/cn.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
+
 #sudo apt-get install libasound-dev libsndfile-dev libsamplerate-dev
 
-for a in *.c ; do
-  b="${a%.*}"
-  echo compiling $b
-  gcc -O3 -Wall $b.c -o $b.bin -lasound -lsndfile -lsamplerate -lm
-  done
+. ../native/i.sh
+
+for fn in playback w64mpg; do
+  compileFile $fn "" "-lasound -lsndfile -lsamplerate" ""
+done
+
+for fn in sender; do
+  compileFile $fn "" "-lsndfile -lsamplerate" ""
+done
+
+for fn in visMeterLoc; do
+  compileFile $fn "" "-lasound -lm" ""
+done
+
+for fn in receiver recorder streamer streamerBth streamerLft streamerRgt w64play w64rec; do
+  compileFile $fn "" "-lasound" ""
+done
+
+for fn in visMeterRem; do
+  compileFile $fn "" "-lm" ""
+done
+
+for fn in forwarder w64fix; do
+  compileFile $fn "" "" ""
+done
